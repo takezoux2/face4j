1620a0c1e96eba197ac77f215b02ef5daaa6ef80
Marlon Hendred <mhendred@gmail.com>
2011-05-26 04:22:37 -0700

    added tests

--- face4j-core/src/test/java/com/github/mhendred/face4j/tests/response/ResponseTests.java
+++ face4j-core/src/test/java/com/github/mhendred/face4j/tests/response/ResponseTests.java
@@ -25,7 +25,6 @@ import com.github.mhendred.face4j.response.TrainResponse;
 import com.github.mhendred.face4j.response.TrainResponseImpl;
 import com.github.mhendred.face4j.tests.BaseTest;
 
-
 public class ResponseTests extends BaseTest
 {
 	public ResponseTests() throws IOException
@@ -38,61 +37,61 @@ public class ResponseTests extends BaseTest
 	{
 		PhotoResponse pr = new PhotoResponseImpl(twoFaces);
 		Photo p = pr.getPhoto();
-		
+
 		assertEquals(p.getFaceCount(), 2);
 	}
-	
+
 	@Test
 	public void trainResponse () throws FaceClientException
 	{
 		TrainResponse tr = new TrainResponseImpl(train);
-		
+
 		assertEquals(1, tr.getNoTrainingSet().size());
-		
+
 		assertTrue(tr.getCreated().isEmpty());
 		assertTrue(tr.getInProgress().isEmpty());
 		assertTrue(tr.getUnchanged().isEmpty());
 		assertTrue(tr.getUpdated().isEmpty());
 	}
-	
+
 	@Test
 	public void removeTagResponse () throws FaceClientException
 	{
 		RemoveTagResponse rtr = new RemoveTagResponseImpl(removeTag);
-		
-		assertEquals(1,rtr.getRemovedTags().size());
+
+		assertEquals(1, rtr.getRemovedTags().size());
 	}
-	
+
 	@Test
 	public void saveTagsResponse () throws FaceClientException
 	{
 		SaveTagResponse str = new SaveTagResponseImpl(saveTag);
-		
+
 		assertEquals(1, str.getSavedTags().size());
 	}
-	
+
 	@Test
 	public void statusResponse () throws FaceClientException
 	{
 		StatusResponse sr = new StatusResponseImpl(status);
-		
+
 		assertEquals(1, sr.getTrainingStatus().size());
 	}
-	
+
 	@Test
 	public void groupRespons () throws Exception
 	{
 		GroupResponse gr = new GroupResponseImpl(group);
-		
+
 		assertEquals(2, gr.getGroups().size());
 		assertEquals(2, gr.getPhotos().size());
 	}
-	
+
 	@Test
 	public void getTagsResponse () throws Exception
 	{
 		GetTagsResponse gt = new GetTagsResponseImpl(tags);
-		
+
 		assertEquals(23, gt.getPhotos().size());
-	}	
+	}
 }
